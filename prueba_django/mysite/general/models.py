51a051c7e3a452f24aca1579717fd476e075da85
salus <salamanca.esteve@gmail.com>
2024-03-25 20:57:40 +0100

    disable 2fa

--- prueba_django/mysite/general/models.py
+++ prueba_django/mysite/general/models.py
@@ -1,3 +1,8 @@
+# models.py
+
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class UserSettings(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    two_factor_auth_enabled = models.BooleanField(default=False)
