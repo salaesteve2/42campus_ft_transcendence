7755b1434912bed03a3f230aaf84ec568465b1aa
valarcon42madrid <108015531+valarcon42madrid@users.noreply.github.com>
2024-03-31 20:15:03 +0200

    alias

--- prueba_django/mysite/general/models.py
+++ prueba_django/mysite/general/models.py
@@ -7,3 +7,4 @@ class UserSettings(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     two_factor_auth_enabled = models.BooleanField(default=False)
     language = models.CharField(max_length=10, choices=[('no', 'No'), ('en', 'English'), ('es', 'Spanish'), ('fr', 'French')], default='no')
+    alias = models.CharField(max_length=50)
