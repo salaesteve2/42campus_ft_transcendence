c0b8fdb2b372adaca7046cfc7539410edc379f0f
valarcon42madrid <108015531+valarcon42madrid@users.noreply.github.com>
2024-04-10 18:06:20 +0200

    Persistence of database's volume

--- prueba_django/mysite/entrypoint.sh
+++ prueba_django/mysite/entrypoint.sh
@@ -11,7 +11,6 @@ then
     echo "PostgreSQL started"
 fi
 
-python manage.py flush --no-input
 python manage.py migrate
 
 python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', '', '$ADMINPASS')"
