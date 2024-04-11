6d5a88da749410e9a853f49d49827778297ac573
valarcon42madrid <108015531+valarcon42madrid@users.noreply.github.com>
2024-04-11 13:33:57 +0200

    Apañado (pero no hay rearanque de torneo) osea que cambio de idioma te hace perder

--- prueba_django/mysite/partidos/views.py
+++ prueba_django/mysite/partidos/views.py
@@ -400,7 +400,11 @@ def fun_rearranque(request, vTipo): # vTipo: "T" = torneo, "R" = rápida
 	# numJugador: 1 = izq, 2 = der
 	result['response'] = render(request, 'partidos/pantallaPong_t.html', mycontext)
 	# enviar el html-javascript que atiende el partido cambiando: idPartido, numJugador, myLanguage, limiteHoraPartido
-	result['ok'] = True
+	if partido.tipo != "T":
+		result['response'] = render(request, 'partidos/pantallaPong_t.html', mycontext)
+		result['ok'] = True
+	else:
+		result['ok'] = False
 	return result
 
 def fun_arranque_torneo(request): #arranque torneo
