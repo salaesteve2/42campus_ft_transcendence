a2fe8330c7edda3d836b7144a7bd61ee7fe7f87a
salaesteve2 <salamanca.esteve@gmail.com>
2024-04-14 21:39:03 +0200

    css update

--- prueba_django/mysite/torneos/forms.py
+++ prueba_django/mysite/torneos/forms.py
@@ -4,24 +4,35 @@ from django import forms
 from django.utils.translation import gettext_lazy as _
 from .models import Torneo
 
-class TorneoForm(forms.Form):  
-    nombre = forms.CharField(max_length=150, label=_('Tournament name'))
+from django import forms
+from django.utils.translation import gettext_lazy as _
+
+class TorneoForm(forms.Form):
+    nombre = forms.CharField(
+        max_length=150, 
+        label=_('Tournament name'),
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
     comienzo_inscripcion = forms.DateTimeField(
-        label=_('Start of registration'), 
-        #widget=forms.widgets.DateInput(attrs={'type': 'datetime', 'placeholder': 'yyyy-mm-dd hh:mm', 'class': 'form-control'})
-        widget=forms.TextInput(attrs={'type':'datetime-local'})
+        label=_('Start of registration'),
+        widget=forms.TextInput(attrs={'type':'datetime-local', 'class': 'form-control'})
     )
     fin_inscripcion = forms.DateTimeField(
         label=_('End of registration'),
-        # widget=forms.widgets.DateInput(attrs={'type': 'datetime', 'placeholder': 'yyyy-mm-dd hh:mm', 'class': 'form-control'})
-        widget=forms.TextInput(attrs={'type':'datetime-local'})
+        widget=forms.TextInput(attrs={'type':'datetime-local', 'class': 'form-control'})
     )
     comienzo_partidos = forms.DateTimeField(
         label=_('Start of matches'),
-        # widget=forms.widgets.DateInput(attrs={'type': 'datetime', 'placeholder': 'yyyy-mm-dd hh:mm', 'class': 'form-control'})
-        widget=forms.TextInput(attrs={'type':'datetime-local'})
+        widget=forms.TextInput(attrs={'type':'datetime-local', 'class': 'form-control'})
     )
-    minutos_duracion_maxima_partidos = forms.IntegerField(label=_('Maximum match duration in minutes'))
-    minutos_entre_partidos = forms.IntegerField(label=_('Minutes between matches'))
+    minutos_duracion_maxima_partidos = forms.IntegerField(
+        label=_('Maximum match duration in minutes'),
+        widget=forms.NumberInput(attrs={'class': 'form-control'})
+    )
+    minutos_entre_partidos = forms.IntegerField(
+        label=_('Minutes between matches'),
+        widget=forms.NumberInput(attrs={'class': 'form-control'})
+    )
+
     class Meta:  
         model = Torneo
